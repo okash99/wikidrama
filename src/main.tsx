832852fd63bd3d4651bc653d98b88c1a57d06187
okash99 <87243299+okash99@users.noreply.github.com>
2026-04-27 20:49:27 +0200

    feat: wrap app with SettingsProvider and i18n

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { clearStaleCache } from './utils/cache'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n'
+import { SettingsProvider } from './context/SettingsContext'
 
-// Purge les anciennes versions de cache au démarrage (v1–v7)
 clearStaleCache()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
+    </I18nextProvider>
   </React.StrictMode>,
 )
